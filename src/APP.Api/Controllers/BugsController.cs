29e367e3036147cbb742455cbefca7c2b37e2577
ali.mousa@z2dataeg.com <ali.mousa@z2dataeg.com>
2023-05-24 09:16:58 +0300

    Create Bugs controller

---
+++ src/APP.Api/Controllers/BugsController.cs
@@ -0,0 +1,58 @@
+﻿using APP.Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APP.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BugsController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public BugsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound()
+        {
+            var product = context.Products.Find(50);
+            if(product is null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            var product = context.Products.Find(50);
+            product.Name = "";
+            return Ok();
+        }
+
+
+
+        [HttpGet("bad-request/id")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+
+
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+
+    }
+}
